91a021114b065588eb5cf4240c574805e129968f
CHARAN HS <charansathish455@gmail.com>
2024-06-30 20:57:57 +0530

    Add files via upload

---
+++ day5.js
@@ -0,0 +1,24 @@
+// const india=  new Number(210)
+// console.log(india)
+// console.log(india.toString().length)
+// console.log(india.toFixed(2)); 
+
+
+// const score= new Number(123.890)
+// console.log(score.toPrecision(4))
+
+// let no=123456789
+// console.log(no.toLocaleString());
+
+
+// console.log(Math.min(1,2,3,4,));
+// console.log(Math.ceil(3.7));
+// console.log(Math.floor(2.6));
+// console.log(Math.max(232,233,234));
+// console.log(Math.floor(Math.random()*10)+ 1)
+
+// const max=30
+// const min=20
+// console.log(Math.floor(Math.random()*min-max))
+
+console.log(Math.abs(-1))
