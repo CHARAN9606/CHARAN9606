76cd8f00467896727e1a3d8e32278c24ed336ec8
CHARAN HS <charansathish455@gmail.com>
2024-07-15 20:27:18 +0530

    Add files via upload

---
+++ day10.js
@@ -0,0 +1,30 @@
+var c =200
+
+
+ if (true) {
+     var c=20
+     let b=20
+     console.log("b:",b)
+
+}
+console.log(c)
+
+ function one(){
+     let  name ="Chs"
+     function two(){
+        let id="Chittalphattal"
+ console.log(name)     }
+console.log(id)
+     two()
+ }
+onsole.log(user);
+
+
+console.log(addone(2))
+function addone(num){
+    return 1+num
+}
+console.log(addtwo(2))
+const addtwo= function(num){
+    return 2+num
+}
