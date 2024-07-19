d6813082fe6c97dac03c0eed820b34cd6767a021
CHARAN HS <charansathish455@gmail.com>
2024-07-19 22:04:51 +0530

    Add files via upload

---
+++ day13.js
@@ -0,0 +1,98 @@
+// if (2!=3){
+//     console.log("true")
+// }else{
+//     console.log("False");
+// }
+
+
+// const high = 1000
+// if(high != 1000){
+//     console.log(`Heavy party`);
+// } else if(high < 900){
+//     console.log("light party");
+// }
+// else if(high < 800){
+//     console.log("light party");
+// }
+// else (high < 250) 
+//     console.log("ok ok")
+
+
+// const Month=4
+// switch (Month) {
+//     case 1:
+//         console.log("jan")
+//         break;
+//     case 2:
+//         console.log("Feb")
+//         break;
+//     case 3:
+//         console.log("March")
+//         break;
+//     case 4:
+//         console.log("April")
+//         break;
+
+//     default:
+//         console.log("out of bound")
+//         break;
+// }
+
+
+
+
+
+// const Months="April"
+// switch (Months) {
+//     case "Jan":
+//         console.log("jan")
+//         break;
+//     case "Feb":
+//         console.log("Feb")
+//         break;
+//     case "Marcc":
+//         console.log("March")
+//         break;
+//     case "April":
+//         console.log("April")
+//         break;
+
+//     default:
+//         console.log("out of bound")
+//         break;
+// }
+
+
+
+
+// const newobj={}
+// if(Object.keys(newobj).length===0){
+//     console.log("Object is empty");
+
+// }
+
+
+
+
+// let user = 0
+// if(user){
+//     console.log("user exists");
+// }else{
+//     console.log("Doesnt Exists");
+// }
+
+
+let v;
+v=null ?? 3
+console.log(v);
+
+
+let n=100
+n>2 ? console.log("false"):console.log("true")
+
+let n1=100;
+if(n1<=200 && 1==1|| n1>50){
+    console.log("100 is greater than 50 and less than 200");
+}else{
+    console.log("Check again");
+}
