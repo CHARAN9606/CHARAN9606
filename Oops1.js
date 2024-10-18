4323917fd0a83604eb2feec28633313fe8b153e5
CHARAN HS <charansathish455@gmail.com>
2024-10-18 22:17:37 +0530

    Add files via upload

---
+++ Oops1.js
@@ -0,0 +1,25 @@
+// function addition(num){
+//     return num*10;
+// }
+// addition.power = 5
+// console.log(addition(2));
+// console.log(addition.power);
+
+// function username(name,userid){
+//     this.name = name;
+//     this.userid = userid;
+// }
+
+// username.prototype.increment = function(){
+//     this.userid++;
+// }
+// username.prototype.printMe = function(){
+//     // console.log(`id ${this.userid}`);
+    
+// }
+// const one = new username('ABC',1)
+
+// one.printMe()
+
+let nameex = "abss   " 
+console.log(nameex.trueLength)
