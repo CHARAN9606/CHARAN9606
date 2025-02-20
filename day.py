6233580e364b32083c0968458b22bdfe15f92475
CHARAN HS <charansathish455@gmail.com>
2025-02-20 21:53:04 +0530

    Add files via upload

---
+++ day.py
@@ -0,0 +1,57 @@
+# print("CHS")
+# print("*"*10)
+#
+# patient_name = "Patient1"
+# patient_age = 20
+# new_patient = True
+# print(patient_name,patient_age,new_patient)
+#
+# name = input("what is your name ? ")
+# fav_color = input("What is your favourite color ? ")
+# print(name +" likes "+ fav_color)
+
+
+# weight = int(input("Enter your weight in lbs ? "))
+# out = (weight)*0.45
+# print("your Weight in grams is : ",out)
+
+# colon = " full high's"
+# colon1 = 'hello "buddy"'
+# three = ('''hello buddy
+#  hi
+#  buddy "light weight baby"''')
+# print(three)
+
+# first = "jhonny"
+# last = "actor"
+# out = f"{first} is a [{last}] ."
+# print(out)
+
+# course = "Light weight buddy"
+# print(course[0:2])
+# print(course.find('L'))
+# print(course.replace('Light','superLight'))
+# print('Light' in course)
+# print(course.title())
+
+
+# print(100 // 3)
+#
+# x = 20
+# x *= 2
+# print(x)
+
+
+
+# x = -28.6
+# print(round(x))
+# print(abs(x))
+
+import math
+
+# x = math.floor(20.5)
+# print(x)
+
+print(math.exp2(3))
+
+math.isnan(float('nan'))
